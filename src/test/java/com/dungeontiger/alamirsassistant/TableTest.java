de8adf97153533a0c5c987cce4f85ef366f1d7b8
dungeontiger <stephen.d.gibson@gmail.com>
2022-07-16 16:03:37 -0400

    New content and tests

--- src/test/java/com/dungeontiger/alamirsassistant/TableTest.java
+++ src/test/java/com/dungeontiger/alamirsassistant/TableTest.java
@@ -3,16 +3,21 @@ package com.dungeontiger.alamirsassistant;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.mockito.stubbing.Answer;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class TableTest {
 
     @Mock
@@ -84,6 +89,52 @@ public class TableTest {
         Table table = tableManager.getTable("test", "test");
         ResponseResult result = table.roll().get(0);
         assertEquals("This is a very nice long note. What do you think? Maybe I should send them a Lich.", result.getText());
+    }
+
+    @Test
+    public void testGreaterUndeadEncounters() {
+        testEncounterTable("TombOfAnnihilation", "GreatUndeadJungleEncounters");
+    }
+
+    private void testEncounterTable(String campaignName, String tableName) {
+        // we want to test all encounters in this table
+        // to do that we always want to roll an encounter, therefore 1d20 is an 18
+        // then for the 1d100 we want to iterate through them
+        // but for all other rolls we want to just roll
+        ResourceTableManager tableManager = new ResourceTableManager(dice, new NLG());
+        Table table = tableManager.getTable(campaignName, tableName);
+        for (int i = 1; i <= 100; i++) {
+            int finalI = i;
+            when(dice.roll(anyString())).thenAnswer(
+                new Answer() {
+                    Dice realDice = new Dice();
+                    public Object answer(InvocationOnMock invocation) {
+                        String dice = invocation.getArgument(0);
+                        if (dice.compareTo("1d20") == 0) {
+                            return 18;
+                        } else if (dice.compareTo("1d100") == 0) {
+                            return finalI;
+                        } else if (dice.isEmpty()) {
+                            return 0;
+                        }
+                        return realDice.roll(dice);
+                    }
+                }
+            );
+            when(dice.roll(anyInt(), anyInt(), anyInt())).thenAnswer(
+              new Answer() {
+                  Dice realDice = new Dice();
+                  public Object answer(InvocationOnMock invocation) {
+                      int dice = invocation.getArgument(0);
+                      int sides = invocation.getArgument(1);
+                      int modifier = invocation.getArgument(2);
+                      return realDice.roll(dice,sides,modifier);
+                  }
+              }
+            );
+            List<ResponseResult> results = table.roll();
+            assertNotNull(results.get(0));
+        }
     }
 }
 
